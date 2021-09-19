bb9331e6a09a99f9d618cc377d0ad57e6112a6b7
Jakub Zich <456431@mail.muni.cz>
2021-09-19 11:25:33 +0200

    formatting

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-let data = [0,0, 0,0,0,0,0, 0];
-data.map(function (val, index) {
-  return index
-})
-
+const data = [0, 0, 0, 0, 0, 0, 0, 0];
+data.map((_val, index) => index);
 const obj = {
-type: "greeting", data: data, value: 'World',  };
-
-
-console.log('Hello ' + obj['value'] + '!! ' + data.join(' > '))
+	type: 'greeting',
+	data,
+	value: 'World'
+};
+console.log(`Hello ${obj.value}!! ${data.join(' > ')}`);
